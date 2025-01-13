115f33f40327d039797e22776c783f70f2cce82d
NixBaBy <tuuguu123123@gmail.com>
2025-01-13 12:31:49 +0800

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
+"use client";
 import Image from "next/image";
 import { Logo } from "@/components/Logo";
 import { Input } from "../components/Input";
 import { Continue } from "@/components/Continue";
+import { useState } from "react";
 export default function Home() {
+  const [inputv, setInputv] = useState();
+  const inputHandler = (e) => {
+    setInputv(e.target.value);
+  };
+
   return (
     <div className="flex w-[480px] h-[655px] p-[32px] flex-col justify-between items-start bg-[#FFF] rounded-[8px]">
       <div className="flex flex-col items-start gap-[12px]">
         <Logo />
-        <Input label={"First Name"} placeholder={"Your First Name"} />
+        <Input
+          label={"First Name"}
+          placeholder={"Your First Name"}
+          inputHandler={inputHandler}
+        />
         <Input label={"Last Name"} placeholder={"Your Last Name"} />
         <Input label={"User Name"} placeholder={"Your User Name"} />
       </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
-export const Input = ({ label, placeholder }) => {
+export const Input = ({ label, placeholder, inputHandler }) => {
   return (
     <div className="flex flex-col gap-[8px]">
       {label}
       <input
         type="text"
+        onChange={inputHandler}
         placeholder={placeholder}
         className="p-[12px] rounded-[8px] border-[1px] border-solid border-[#gray] w-[416px] focus:outline-none focus:border-sky-500"
       />
